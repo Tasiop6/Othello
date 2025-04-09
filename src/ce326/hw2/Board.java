2b38fb667f235f5dff4fd94051d9b196c22326d7
Konstantinos Tasiopoulos <ktasiopoulos@satways.net>
2025-04-09 16:49:36 +0300

    First changes

---
+++ src/ce326/hw2/Board.java
@@ -0,0 +1,10 @@
+package ce326.hw2;
+
+public class Board {
+    private Pawn[][] board;
+
+    public Board() {
+        board = new Pawn[8][8];
+        // Initialization logic will be added later
+    }
+}
